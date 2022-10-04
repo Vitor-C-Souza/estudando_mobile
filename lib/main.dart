7a264c8fe9b26fdc1bf3b7a7ef8cd74ffea7cccc
Makinay <vitor_souza97@hotmail.com>
2022-10-04 12:27:52 -0300

    Animação, bordas e imagens adicionadas com sucesso.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -19,18 +26,54 @@ class MyApp extends StatelessWidget {
           leading: Container(),
           title: Text('Tarefas'),
         ),
-        body: ListView(
-          children: [
-            Task('Aprender Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
-            Task('Andar de bike', 'https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/d7/46/d8/d746d8ca-3e6d-b096-71b0-71dbac1c0514/source/256x256bb.jpg'),
-            Task('Meditar', 'https://cdn-icons-png.flaticon.com/128/3663/3663335.png'),
-            Task('Cozinhar', 'https://images.vexels.com/media/users/3/224170/isolated/lists/82bbb2639aacf6d2a71c89e84c87aa5f-logotipo-de-ingredientes-para-cozinhar.png'),
-            Task('League Of Legends', 'https://i.pinimg.com/originals/24/a5/61/24a56146dfd05b942acab219b6dfa985.png'),
-            Task('NodeJS', 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png'),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: Duration(milliseconds: 1000),
+          child: ListView(
+            children: [
+              Task(
+                'Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                3,
+              ),
+              Task(
+                'Andar de bike',
+                'https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/d7/46/d8/d746d8ca-3e6d-b096-71b0-71dbac1c0514/source/256x256bb.jpg',
+                1,
+              ),
+              Task(
+                'Meditar',
+                'https://cdn-icons-png.flaticon.com/128/3663/3663335.png',
+                4,
+              ),
+              Task(
+                'Cozinhar',
+                'https://images.vexels.com/media/users/3/224170/isolated/lists/82bbb2639aacf6d2a71c89e84c87aa5f-logotipo-de-ingredientes-para-cozinhar.png',
+                2,
+              ),
+              Task(
+                'League Of Legends',
+                'https://i.pinimg.com/originals/24/a5/61/24a56146dfd05b942acab219b6dfa985.png',
+                5,
+              ),
+              Task(
+                'NodeJS',
+                'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png',
+                5,
+              ),
+            ],
+          ),
         ),
-        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState((){
+              opacidade = !opacidade;
+            });
+
+
+          },
+          child: Icon(Icons.remove_red_eye),
+        ),
       ),
     );
   }
@@ -39,8 +82,10 @@ class MyApp extends StatelessWidget {
 class Task extends StatefulWidget {
   final String nome;
   final String foto;
+  final int dificuldade;
 
-  const Task(this.nome, this.foto, {Key? key}) : super(key: key);
+  const Task(this.nome, this.foto, this.dificuldade, {Key? key})
+      : super(key: key);
 
   @override
   State<Task> createState() => _TaskState();
@@ -57,35 +102,92 @@ class _TaskState extends State<Task> {
         child: Stack(
           children: [
             Container(
-              color: Colors.blue,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: Colors.blue,
+              ),
               height: 140,
             ),
             Column(
               children: [
                 Container(
-                  color: Colors.white,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.white,
+                  ),
                   height: 100,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Container(
-                        color: Colors.black26,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(4),
+                          color: Colors.black26,
+                        ),
                         width: 72,
                         height: 100,
-                        child: Image.network(
-                          widget.foto,
-                          fit: BoxFit.cover,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(4),
+                          child: Image.network(
+                            widget.foto,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                      Container(
-                          width: 200,
-                          child: Text(
-                            widget.nome,
-                            style: TextStyle(
-                              fontSize: 24,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          )),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                              width: 200,
+                              child: Text(
+                                widget.nome,
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 1)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 2)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 3)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade >= 4)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                              Icon(
+                                Icons.star,
+                                size: 15,
+                                color: (widget.dificuldade == 5)
+                                    ? Colors.blue
+                                    : Colors.blue[100],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                       Container(
                         height: 52,
                         width: 52,
@@ -120,7 +222,9 @@ class _TaskState extends State<Task> {
                       child: Container(
                         child: LinearProgressIndicator(
                           color: Colors.white,
-                          value: nivel / 15,
+                          value: (widget.dificuldade > 0)
+                              ? (nivel / widget.dificuldade) / 10
+                              : 1,
                         ),
                         width: 200,
                       ),
