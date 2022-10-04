a0a2974e8fb5ab63dcc036973a64014cc1d6977a
Makinay <vitor_souza97@hotmail.com>
2022-10-04 00:59:51 -0300

    imagens adicionadas

--- lib/main.dart
+++ lib/main.dart
@@ -16,20 +16,18 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
+          leading: Container(),
           title: Text('Tarefas'),
         ),
         body: ListView(
           children: [
-            Task('Aprender Flutter'),
-            Task('Andar de bike'),
-            Task('Meditar'),
-            Task('Meditar'),
-            Task('Meditar'),
-            Task('Meditar'),
-            Task('Meditar'),
-            Task('Meditar'),
-            Task('Meditar'),
-            Task('Meditar'),
+            Task('Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+            Task('Andar de bike', 'https://is3-ssl.mzstatic.com/image/thumb/Purple113/v4/d7/46/d8/d746d8ca-3e6d-b096-71b0-71dbac1c0514/source/256x256bb.jpg'),
+            Task('Meditar', 'https://cdn-icons-png.flaticon.com/128/3663/3663335.png'),
+            Task('Cozinhar', 'https://images.vexels.com/media/users/3/224170/isolated/lists/82bbb2639aacf6d2a71c89e84c87aa5f-logotipo-de-ingredientes-para-cozinhar.png'),
+            Task('League Of Legends', 'https://i.pinimg.com/originals/24/a5/61/24a56146dfd05b942acab219b6dfa985.png'),
+            Task('NodeJS', 'https://cdn.iconscout.com/icon/free/png-256/node-js-1174925.png'),
           ],
         ),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
@@ -38,10 +36,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   final String nome;
+  final String foto;
 
-  const Task(this.nome, {Key? key}) : super(key: key);
+  const Task(this.nome, this.foto, {Key? key}) : super(key: key);
+
+  @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  int nivel = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -66,22 +72,68 @@ class Task extends StatelessWidget {
                         color: Colors.black26,
                         width: 72,
                         height: 100,
+                        child: Image.network(
+                          widget.foto,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                       Container(
                           width: 200,
                           child: Text(
-                              nome,
-                              style: TextStyle(
-                                fontSize: 24,
-                                overflow: TextOverflow.ellipsis,
-                              ),
+                            widget.nome,
+                            style: TextStyle(
+                              fontSize: 24,
+                              overflow: TextOverflow.ellipsis,
+                            ),
                           )),
-                      ElevatedButton(
-                          onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                      Container(
+                        height: 52,
+                        width: 52,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nivel++;
+                              });
+
+                              print(nivel);
+                            },
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Icon(Icons.arrow_drop_up),
+                                Text(
+                                  'UP',
+                                  style: TextStyle(fontSize: 12),
+                                )
+                              ],
+                            )),
+                      )
                     ],
                   ),
                 ),
-                Text('nivel 0')
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        child: LinearProgressIndicator(
+                          color: Colors.white,
+                          value: nivel / 15,
+                        ),
+                        width: 200,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text(
+                        'nivel $nivel',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ],
+                )
               ],
             )
           ],
