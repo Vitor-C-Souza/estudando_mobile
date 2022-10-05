e9ddee2b9242e24b78720c20057fe58802f68ee5
Makinay <vitor_souza97@hotmail.com>
2022-10-05 02:45:33 -0300

    Nova tela encaminhada e aula sobre formulario concluida.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:estudando_mobile/screens/initial_screen.dart';
+import 'package:estudando_mobile/screens/form_screen.dart';
+// import 'package:estudando_mobile/screens/initial_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const InitalScreen(),
+      home: const FormScreen(),
     );
   }
 }
